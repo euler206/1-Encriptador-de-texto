edb8c9d0246b404231c09cb10c01728ea9a9c869
Euler Basante <euler206@gmail.com>
2023-04-12 07:33:18 -0500

    no corregido

--- js/logica.js
+++ js/logica.js
@@ -1,15 +1,19 @@
 
 function Encriptar(){
     let texto = document.getElementById("texto").value;
-    let encriptado = "";
-    for (var i = 0; i < texto.length; i++) {
-        encriptado += String.fromCharCode(texto.charCodeAt(i) + 1);
-    }
+
+    let encriptado = texto + "este  texto se encuentra encriptado.";
     document.getElementById("textoEncriptado").value = encriptado;   
 }
 
+const btnEncriptar = document.getElementById("btnencriptar");
+btnEncriptar.onclick = Encriptar;
+
+
+/*
 function Desencriptar(){
     let texto = document.getElementById("textoEncriptado").value;
     let desencriptado = "";
     document.getElementById("textoEncriptado").value = "TEXTO DESENCRIPTADO.";
 }
+*/
