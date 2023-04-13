9a6afb75141af7a29c6a3068f96eaf2829a9b505
Euler Basante <euler206@gmail.com>
2023-04-12 22:17:46 -0500

    Updates

---
+++ js/logica.js
@@ -0,0 +1,29 @@
+const texto = document.getElementById("texto").value;
+const btn = document.getElementById("btn");
+const txtEncriptado = document.getElementById("textoEncriptado");
+
+btn.onclick = function () {
+    txtEncriptado.value = "";
+    txtEncriptado.value = encriptar(texto);
+};
+
+
+function encriptar(texto){
+    let llave = [["e" , "enter"],["i" , "imes"],["a" , "ai"],["o" , "ober"],["u" , "ufat"]];
+    texto = texto.toLowerCase();
+    for (let i=0; i<llave.length; i++){
+        if(texto.includes(llave[i][0])){
+            texto = texto.replaceAll(llave[i][0], llave[i][1]);
+        }
+
+    }
+    console.log(texto);
+   return texto;
+}
+/*
+function Desencriptar(){
+    let texto = document.getElementById("textoEncriptado").value;
+    let desencriptado = "";
+    document.getElementById("textoEncriptado").value = "TEXTO DESENCRIPTADO.";
+}
+*/
