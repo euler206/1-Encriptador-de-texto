a73341fad787fe147a0f1e07fee7fa8b36d040ca
Euler Basante <euler206@gmail.com>
2023-04-12 21:55:55 -0500

    encripta mal

--- js/logica.js
+++ js/logica.js
@@ -1,20 +1,26 @@
-const texto = document.getElementById("texto");
-const btn = document.querySelector("btn");
+const texto = document.getElementById("texto").value;
+const btn = document.getElementById("btn");
 const txtEncriptado = document.getElementById("textoEncriptado");
 
-document.getElementById("btn").onclick = function () {
-  textoEncriptado.value = texto.value +"TEXTO";
-  console.log(textoEncriptado.value)
+btn.onclick = function () {
+    txtEncriptado.value = "";
+    txtEncriptado.value = encriptar(texto);
 };
 
-/*
-function encriptar(){
 
-    let encriptado = texto.value + "este  texto se encuentra encriptado.";
-    txtEncriptado.value = encriptado;
-    alert("Texto encriptado con exito.");   
+function encriptar(texto){
+    let llave = [["e" , "enter"],["i" , "imes"],["a" , "ai"],["o" , "ober"],["u" , "ufat"]];
+    texto = texto.toLowerCase();
+    for (let i=0; i<llave.length; i++){
+        if(texto.includes(llave[i][0])){
+            texto = texto.replaceAll(llave[i][0], llave[i][1]);
+        }
+
+    }
+    console.log(texto);
+   return texto;
 }
-
+/*
 function Desencriptar(){
     let texto = document.getElementById("textoEncriptado").value;
     let desencriptado = "";
