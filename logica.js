6592a832e98f3170b20a190d37dc5fb0d94e0d4c
Euler Basante <euler206@gmail.com>
2023-04-14 23:03:51 -0500

    logica refacturada

--- logica.js
+++ logica.js
@@ -1,21 +1,33 @@
 import llave from "./key.js";
 const texto = document.getElementById("texto");
-const btnbtnEncriptar = document.getElementById("btnEncriptar"); 
+const btnEncriptar = document.getElementById("btnEncriptar"); 
 const btnDesencriptar = document.getElementById("btnDesencriptar"); 
 const txtEncriptado = document.getElementById("textoEncriptado");
 
+function encriptar(mensaje,op) {
+    mensaje = mensaje.toLowerCase();
+    llave.forEach(([enc, des]) => mensaje = op ? mensaje.replaceAll(enc, des): mensaje.replaceAll(des, enc));
+    return mensaje;
+};
+
+btnEncriptar.onclick = () => txtEncriptado.value = encriptar(texto.value,1); // 1 = true para encriptar
+btnDesencriptar.onclick = () => txtEncriptado.value = encriptar(texto.value,0); // 0 = false para desencriptar
+
+
+
+
+
+
+
+/*
 function encriptar(mensaje) {
     mensaje = mensaje.toLowerCase();
     llave.forEach(([enc, des]) => mensaje = mensaje.replaceAll(enc, des));
     return mensaje;
 };
-
 function desencriptar(mensaje) {
     mensaje = mensaje.toLowerCase();
     llave.forEach(([enc, des]) => mensaje = mensaje.replaceAll(des, enc));
     return mensaje;
 };
-
-btnbtnEncriptar.onclick = () => txtEncriptado.value = encriptar(texto.value);
-btnDesencriptar.onclick = () => txtEncriptado.value = desencriptar(texto.value);
-
+*/
