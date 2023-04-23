b4baf7837ce40d36e4a5d6eb22405b06f76d1688
Euler Basante <euler206@gmail.com>
2023-04-23 15:31:47 -0500

    correcipones

--- logica.js
+++ logica.js
@@ -22,3 +22,18 @@ function pegar() {
 btnEncriptar.onclick = () => txtEncriptado.value = encriptar(texto.value,1); // 1 = true para encriptar
 btnDesencriptar.onclick = () => txtEncriptado.value = encriptar(texto.value,0); // 0 = false para desencriptar
 btnCopiar.onclick = () => btnCopiar.innerText === "Copiar" ? copiar() : pegar();
+
+
+
+
+/* ************* CODIGO AUN MAS COMPACTO **************
+import llave from "./key.js";
+const [texto, btnEncriptar, btnDesencriptar, txtEncriptado, btnCopiar] = ["texto", "btn-encriptar", "btn-desencriptar", "texto-encriptado", "btn-copiar"].map(id => document.getElementById(id));
+const encriptar = (mensaje, op) => llave.forEach(([enc, des]) => mensaje = op ? mensaje.replaceAll(enc, des) : mensaje.replaceAll(des, enc)), mensaje.toLowerCase();
+const copiar = () => navigator.clipboard.writeText(txtEncriptado.value), btnCopiar.innerText = "Pegar";
+const pegar = () => navigator.clipboard.readText().then(tex => texto.value = tex), btnCopiar.innerText = "Copiar";
+
+btnEncriptar.onclick = () => txtEncriptado.value = encriptar(texto.value, 1);
+btnDesencriptar.onclick = () => txtEncriptado.value = encriptar(texto.value, 0);
+btnCopiar.onclick = () => btnCopiar.innerText === "Copiar" ? copiar() : pegar();
+*/
