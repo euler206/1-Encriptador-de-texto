e08c7e066249cede9523aa7379e27799825ea3f4
Euler Basante <euler206@gmail.com>
2023-04-29 15:23:44 -0500

    arreglado algo de css

--- logica.js
+++ logica.js
@@ -1,30 +1,28 @@
 import llave from "./key.js";
-//const [texto, btnEncriptar, btnDesencriptar, txtEncriptado, btnCopiar] = ["texto", "btn-encriptar", "btn-desencriptar", "texto-encriptado", "btn-copiar"].map(id => document.getElementById(id));
-const texto = document.getElementById("texto");
+//const [texto, btnEncriptar, btnDesencriptar, txtEncriptado, btnCopiar] = ["texto", "btn-encriptar", "btn-desencriptar", "texto-encriptado", "btn-copiar"].map(id => document.getElementById(id)); // poco entendible 
+const texto = document.getElementById("text-area");
 const btnEncriptar = document.getElementById("btn-encriptar"); 
 const btnDesencriptar = document.getElementById("btn-desencriptar"); 
-const txtEncriptado = document.getElementById("texto-encriptado");
-const btnCopiar = document.getElementById("btn-copiar");
+const mensaje = document.getElementById("mensaje");
+const btnCopiarPegar = document.getElementById("btn-copiar-pegar");
 
-const copiar = () => navigator.clipboard.writeText(txtEncriptado.value);
-const pegar = () => navigator.clipboard.readText().then(tex => texto.value = tex);
-function encriptar(mensaje,op) {
-    mensaje = mensaje.toLowerCase();
-    llave.forEach(([enc, des]) => mensaje = op ? mensaje.replaceAll(enc, des): mensaje.replaceAll(des, enc));
-    return mensaje;
+function encriptar(text,op) {
+    text = text.toLowerCase();
+    llave.forEach(([enc, des]) => text = op ? text.replaceAll(enc, des): text.replaceAll(des, enc));
+    return text;
 };
-btnEncriptar.onclick = () => txtEncriptado.value = encriptar(texto.value,1); // 1 = true para encriptar
-btnDesencriptar.onclick = () => txtEncriptado.value = encriptar(texto.value,0); // 0 = false para desencriptar
-btnCopiar.onclick = function () {
-        if (btnCopiar.innerText === "Copiar"){
+const copiar = () => navigator.clipboard.writeText(mensaje.value);
+const pegar = () => navigator.clipboard.readText().then(tex => texto.value = tex);
+btnEncriptar.onclick = () => mensaje.value = encriptar(texto.value,1); // 1 = true para encriptar
+btnDesencriptar.onclick = () => mensaje.value = encriptar(texto.value,0); // 0 = false para desencriptar
+btnCopiarPegar.onclick = function () {
+        if (btnCopiarPegar.innerText === "Copiar"){
             copiar();
-            btnCopiar.innerText = "Pegar";
+            btnCopiarPegar.innerText = "Pegar";
+            mensaje.value = "";
         } else{
             pegar();
-            btnCopiar.innerText = "Copiar";
+            btnCopiarPegar.innerText = "Copiar";
         }
     };
-
-
-
 
